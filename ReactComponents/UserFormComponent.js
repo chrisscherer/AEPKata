c9874f3e40e84dc51ee7bddfbf91e812af00c00d
DESKTOP-LJAFSF7\CScherer <chrisscherer@improving.com>
2019-12-05 06:44:23 -0500

    Updated form to use proper controls

--- ReactComponents/UserFormComponent.js
+++ ReactComponents/UserFormComponent.js
@@ -4,25 +4,60 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import DatePicker from 'react-native-datepicker'
 
 export default class UserFormComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {location: '', itemNumber: '', date: '', description: ''};
   }
 
   render() {
     return (
       <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Enter Location"
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+        <View>
+        <Text>Location: </Text>
+            <TextInput
+              style={{height: 40}}
+              placeholder="Enter Location"
+              onChangeText={(location) => this.setState({location})}
+              value={this.state.location}
+            />
+        </View>
+        <View>
+            <Text>Item Number: </Text>
+            <TextInput
+              style={{height: 40}}
+              placeholder="Enter Item Number"
+              onChangeText={(itemNumber) => this.setState({itemNumber})}
+              value={this.state.itemNumber}
+              keyboardType={'numeric'}
+            />
+        </View>
+        <View>
+            <Text>Date: </Text>
+            <DatePicker
+                    style={{width: 200}}
+                    date={this.state.date}
+                    mode="date"
+                    placeholder="select date"
+                    format="YYYY-MM-DD"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    onDateChange={(date) => {this.setState({date: date})}}
+                  />
+        </View>
+        <View>
+            <Text>Description: </Text>
+            <TextInput
+              style={{height: 120}}
+              placeholder="Enter Description"
+              onChangeText={(description) => this.setState({description})}
+              value={this.state.description}
+              multiline = {true}
+              numberOfLines = {4}
+            />
+        </View>
       </View>
     );
   }
